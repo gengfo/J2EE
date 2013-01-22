aac13cc21f71d242be7f71594379a734e70a91f4
Forest Geng <gengfo@gmail.com>
2013-01-22 13:32:37 +0800

    update

---
+++ common-oc4j-ear/src/main/java/com/oocl/ivo/webservice/publish/IVOWebService.java
@@ -0,0 +1,7 @@
+package com.oocl.ivo.webservice.publish;
+
+public interface IVOWebService {
+    
+    public String helloOc4j(String ttt);
+
+}
---
+++ common-oc4j-ear/src/main/java/com/oocl/ivo/webservice/publish/IVOWebServiceImpl.java
@@ -0,0 +1,12 @@
+package com.oocl.ivo.webservice.publish;
+
+
+public class IVOWebServiceImpl implements IVOWebService {
+
+	public String helloOc4j(String ttt)  {
+
+		
+
+		return "Hello " + ttt;
+	}
+}
